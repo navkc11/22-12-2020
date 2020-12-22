26ff0cea1c0e6666713140d5011715d1f36a0b93
navkc11 <63931144+navkc11@users.noreply.github.com>
2020-12-22 22:42:24 +0200

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Point {
+
+    Double width;
+    Double height;
+    Scanner s = new Scanner(System.in);
+    //banai
+    public Point(Double width, Double height){
+        this.width = width;
+        this.height = height;
+    }
+    //method 1
+    public Point middle(Point x){
+        Point y = new Point(s.nextDouble(),s.nextDouble());
+        double width = (x.width = y.width)/2;
+        double height = (x.height = y.height)/2;
+        Point z = new Point(width, height);
+        return z;
+    }
+    // method 2
+    public Double distance(Point x){
+        Point y = new Point(s.nextDouble(),s.nextDouble());
+        return Math.sqrt(Math.pow((x.height - y.height),2)+Math.pow((x.width - y.width),2));
+    }
+    //method 3
+    public Double areaSize(Point x, Point y){
+        Point z = new Point(s.nextDouble(),s.nextDouble());
+        return (x.width*(y.height-z.height)+y.width*(x.height-z.height)+ z.width*(x.height-y.height))/2;
+    }
+    //method 4
+    public Double slope(){
+        Point x = new Point(s.nextDouble(),s.nextDouble());
+        return x.height/x.width;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Student {
+    String name;
+    ArrayList<Integer> grades;
+    Scanner s = new Scanner(System.in);
+
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = grades;
+    }
+    //method 1
+    public Double studentAvg(){
+        double total = 0;
+        for(int i = 0; i<grades.size()-1;i++){
+            total += grades.get(i);
+        }
+        return total/grades.size();
+    }
+    //method 2
+    public Boolean isTop(){
+        if(studentAvg() >= 90) return true;
+        else return false;
+    }
+    //method 3
+    public Student better(Student other){
+        Student a = new Student(s.next());
+        if (a.studentAvg() > other.studentAvg()) return a;
+        else return other;
+    }
+    //method 4
+    public ArrayList<Integer> failures(){
+        ArrayList<Integer> failures = new ArrayList<Integer>();
+        for(int i = 0; i<grades.size()-1;i++){
+            if(grades.get(i)<=55) {
+                failures.add(grades.get(i));
+            }
+        }
+        return failures;
+    }
+}
